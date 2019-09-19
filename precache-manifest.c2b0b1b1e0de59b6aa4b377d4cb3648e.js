7d810be9eb132da3520d2eb9ae097715677fc4a8
mlkaufman14 <mlkaufman14@gmail.com>
2019-09-19 08:07:10 -0500

    Updates

---
+++ precache-manifest.c2b0b1b1e0de59b6aa4b377d4cb3648e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e7a95e87ea82be794b0ddcb3bb833f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a0932e808db9a2b6e313",
+    "url": "/monsters-rolodex/static/css/main.465d1822.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "a0932e808db9a2b6e313",
+    "url": "/monsters-rolodex/static/js/main.9a41a1bc.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
